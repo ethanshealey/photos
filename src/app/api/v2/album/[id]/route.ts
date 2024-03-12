5348aec2958da7825c1f041df9ec3e2fc6c00c4c
ethanshealey <ethan.shealey@gmail.com>
2024-03-11 23:36:50 -0400

    remove imgur endpoints

--- src/app/api/v2/album/[id]/route.ts
+++ src/app/api/v2/album/[id]/route.ts
@@ -1,22 +1,22 @@
-import { NextRequest, NextResponse } from "next/server"
-import { ApiPageProps } from "@/helpers/ApiPageProps"
-import axios, { AxiosHeaders } from "axios"
-import getImgurAuthorization from "@/helpers/getImgurAuthorization"
+// import { NextRequest, NextResponse } from "next/server"
+// import { ApiPageProps } from "@/helpers/ApiPageProps"
+// import axios, { AxiosHeaders } from "axios"
+// import getImgurAuthorization from "@/helpers/getImgurAuthorization"
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
-export async function GET(req: NextRequest, { params: { id } }: Pick<ApiPageProps, 'params'>): Promise<Response> {
+// export async function GET(req: NextRequest, { params: { id } }: Pick<ApiPageProps, 'params'>): Promise<Response> {
 
-  const headers = getImgurAuthorization()
+//   const headers = getImgurAuthorization()
 
-  let response = await axios.get(`https://api.imgur.com/3/album/${id}`, {
-    headers: headers
-  })
-  response = await response.data
+//   let response = await axios.get(`https://api.imgur.com/3/album/${id}`, {
+//     headers: headers
+//   })
+//   response = await response.data
   
-  console.log(response.data.images)
+//   console.log(response.data.images)
 
-  return new NextResponse(JSON.stringify({
-    image_urls: response.data.images
-  }))
-}
+//   return new NextResponse(JSON.stringify({
+//     image_urls: response.data.images
+//   }))
+// }
--- src/app/api/v2/albums/route.ts
+++ src/app/api/v2/albums/route.ts
@@ -1,25 +1,25 @@
-import { NextRequest, NextResponse } from "next/server"
-import albums from '@/data/albums.json'
-import getImgurAuthorization from "@/helpers/getImgurAuthorization"
-import axios from "axios"
+// import { NextRequest, NextResponse } from "next/server"
+// import albums from '@/data/albums.json'
+// import getImgurAuthorization from "@/helpers/getImgurAuthorization"
+// import axios from "axios"
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
-export async function GET(req: NextRequest): Promise<Response> {
+// export async function GET(req: NextRequest): Promise<Response> {
 
-  const headers = getImgurAuthorization()
+//   const headers = getImgurAuthorization()
 
-  const albumsInfo: any[] = []
+//   const albumsInfo: any[] = []
 
-  for await (const id of albums) {
-    let response = await axios.get(`https://api.imgur.com/3/album/${id}`, { headers: headers })
-    response = await response.data
-    albumsInfo.push(response.data)
-  }
+//   for await (const id of albums) {
+//     let response = await axios.get(`https://api.imgur.com/3/album/${id}`, { headers: headers })
+//     response = await response.data
+//     albumsInfo.push(response.data)
+//   }
   
-  console.log(albumsInfo)
+//   console.log(albumsInfo)
 
-  return new NextResponse(JSON.stringify({
-    albumsInfo
-  }))
-}
+//   return new NextResponse(JSON.stringify({
+//     albumsInfo
+//   }))
+// }
